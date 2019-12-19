ab0b7aa7a361ee1c59c02b23f0c9e78e6651d354
Omer Karakas <Administrator@CBYG-DAN-0QL82W.bilgeadam.com>
2019-12-19 14:23:25 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <CountryNavigator/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CountryNavigator from './components/CountryNavigator'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CountryNavigator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ body{
+   background-color: red;
+ }
 </style>
---
+++ src/components/CountryNavigator.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1>Country Navigator</h1>
+
+	<ul class="list-group">
+		<li  v-for="country in data.countries" v-bind:key="country" class="list-group-item">
+			{{country.name}}
+		</li>
+	</ul>
+
+</div>
+</template>
+
+
+<script>
+import data from '../data/data';
+export default {
+	name: "CountryNavigator",
+	data(){
+		return {
+			data
+		}
+
+	}	
+}
+</script>
+
+
+<style scoped>
+
+</style>
